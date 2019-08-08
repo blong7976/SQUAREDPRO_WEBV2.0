8640e99800e5cf6afb805ca651228e9f0c19bbd2
blong7976 <2456399912@qq.com>
2019-08-08 14:07:51 +0800

    Signed-off-by: blong7976 <2456399912@qq.com>

--- js/hallooou.js
+++ js/hallooou.js
@@ -152,7 +152,7 @@ $(function () {
     var owl = $(".team-project-carousel");
     owl.owlCarousel({
         autoPlay: 3000,
-        items: 3, //5 items above 1000px browser width
+        items: 2, //5 items above 1000px browser width
         itemsDesktop: [1024, 4], //4 items between 1000px and 901px
         itemsDesktopSmall: [900, 2], // betweem 900px and 601px
         itemsTablet: [600, 2], //2 items between 600 and 480
