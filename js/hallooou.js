9eea14b923e4f3c8e3879321fe8a4d73621d9125
blong7976 <2456399912@qq.com>
2019-08-13 17:25:09 +0800

    Signed-off-by: blong7976 <2456399912@qq.com>

--- js/hallooou.js
+++ js/hallooou.js
@@ -1,10 +1,10 @@
 // jQuery to collapse the navbar on scroll
 function resetScroll() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-fixed-top,#language_pc").addClass("top-nav-collapse");
         $(".scroll-top").fadeIn('1000', "easeInOutExpo");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top,#language_pc").removeClass("top-nav-collapse");
         $(".scroll-top").fadeOut('1000', "easeInOutExpo");
     }
 }
